d00169b60f8162a4e848598e607e177f6bb7bae3
Glowdragon <info@glowdragon.de>
2020-01-31 23:00:00 +0100

    -

---
+++ GGJ2020Client/Assets/Scripts/General/ExecutableBuilderTool.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Util
+{
+    public class ExecutableBuilderTool : MonoBehaviour
+    {
+        [SerializeField] private string[] scenes;
+        [SerializeField] private string buildsFolder;
+        [SerializeField] private string executableName;
+        [SerializeField] private bool runAfterBuild;
+
+        public string[] Scenes
+        {
+            get { return scenes; }
+        }
+
+        public string BuildsFolder
+        {
+            get { return buildsFolder; }
+        }
+
+        public string ExecutableName
+        {
+            get { return executableName; }
+        }
+
+        public bool RunAfterBuild
+        {
+            get { return runAfterBuild; }
+        }
+
+        // TODO
+        public bool PackAfterBuild
+        {
+            get { return false; }
+        }
+    }
+}
