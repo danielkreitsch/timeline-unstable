7d9ba37fe3cb345f8172c403259ea9d4cc3688cf
hapill <s1hapill@uni-bayreuth.de>
2020-02-01 19:46:58 +0100

    Added VisibleLog

---
+++ GGJ2020/Assets/visibleLog.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class visibleLog : MonoBehaviour
+{
+
+	string myLog;
+	Queue myLogQueue = new Queue();
+	private int lines = 0;
+
+	// Start is called before the first frame update
+	void OnEnable()
+    {
+	    Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+	    Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+	    myLog = logString;
+	    string newString = "\n [" + type + "] : " + myLog;
+	    myLogQueue.Enqueue(newString);
+	    if (type == LogType.Exception)
+	    {
+		    newString = "\n" + stackTrace;
+		    myLogQueue.Enqueue(newString);
+	    }
+	    myLog = string.Empty;
+	    foreach (string mylog in myLogQueue)
+	    {
+		    myLog += mylog;
+	    }
+
+	    var Text = GetComponent<Text>();
+	    Text.text = myLog;
+	    lines++;
+	    if (lines > 30)
+	    {
+		    lines = 0;
+		    Text.text = "";
+	    }
+    }
+}
