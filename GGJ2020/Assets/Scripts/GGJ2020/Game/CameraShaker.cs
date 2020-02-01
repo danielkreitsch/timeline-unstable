9b6e684347c053ef1f1a7caad9b4b5647404a379
Robin Daraban <robin.daraban@gmx.de>
2020-02-01 20:33:32 +0100

    Add Dispaly and CameraShake

---
+++ GGJ2020/Assets/Scripts/GGJ2020/Game/CameraShaker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    Vector3 startingPosition;
+
+    [SerializeField, Range(0, 1)]
+    float intensity = 0.1f;
+    [SerializeField, Range(0, 1)]
+    float duration = 0;
+    Camera cam;
+
+
+    // Start is called before the first frame update
+    private void Awake() {
+        startingPosition = transform.position;
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        duration -= Time.deltaTime;
+        if (duration > 0) {
+            cam.transform.position = startingPosition + Random.insideUnitSphere * intensity;
+        } else {
+            cam.transform.position = startingPosition;
+        }
+    }
+
+    public void Shake(float duration) {
+        this.duration = duration;
+    }
+}
---
+++ GGJ2020/Assets/Scripts/GGJ2020/Game/DisplayTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class DisplayTime : MonoBehaviour
+{
+    [SerializeField]
+    Game game;
+    [SerializeField]
+    float initialSeconds = 20;
+    [SerializeField]
+    TextMeshPro text;
+    TimeSpan timeSpan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSpan = TimeSpan.FromSeconds(initialSeconds - game.Timer);
+        text.text = timeSpan.ToString(@"ss\:fff");
+    }
+}
