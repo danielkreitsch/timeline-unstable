6b3030b674be81c23a1975af9d886faeb7a68eda
Robin Daraban <robin.daraban@gmx.de>
2020-02-02 06:25:14 +0100

    Sound now on game start

--- GGJ2020/Assets/Scripts/GGJ2020/Sound/RandomClipOnAwake.cs
+++ GGJ2020/Assets/Scripts/GGJ2020/Sound/RandomClipOnAwake.cs
@@ -7,8 +7,14 @@ public class RandomClipOnAwake : MonoBehaviour
 {
     [SerializeField]
     AudioClip[] clips;
+    [SerializeField]
+    Game game;
 
     private void Awake() {
+        game.onGameStart.AddListener(PlaySound);
+    }
+
+    void PlaySound() {
         int random = Random.Range(0, clips.Length);
         GetComponent<AudioSource>().PlayOneShot(clips[random]);
     }
