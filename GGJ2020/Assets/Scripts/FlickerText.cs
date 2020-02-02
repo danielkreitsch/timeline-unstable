18bcd31f961d01af590793a1c8e3efbaca632f86
Robin Daraban <robin.daraban@gmx.de>
2020-02-02 12:02:16 +0100

    Add menu

---
+++ GGJ2020/Assets/Scripts/FlickerText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FlickerText : MonoBehaviour
+{
+
+    [SerializeField]
+    float probabilityThreshhold;
+    [SerializeField]
+    TextMeshPro text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 1f) > probabilityThreshhold) {
+            text.gameObject.SetActive(false);
+        } else {
+            text.gameObject.SetActive(true);
+        }
+    }
+}
